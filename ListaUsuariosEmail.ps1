c6dbfb8e83cecb16deb1f90c4ebaf134395e0bd4
Thiago Passamani <thiagopassamani@gmail.com>
2026-04-14 14:49:46 -0300

    Add files via upload

---
+++ ListaUsuariosEmail.ps1
@@ -0,0 +1,12 @@
+﻿# Importa o módulo Active Directory
+Import-Module ActiveDirectory
+
+# Define a OU alvo
+$OU = "OU=Matriz - ES,OU=NovaForma,OU=Empresas,DC=novaforma,DC=local"
+
+# Busca todos os usuários na OU e sub-OUs
+Get-ADUser -SearchBase $OU -SearchScope Subtree -Filter * -Properties mail |
+Select-Object Name, mail |
+#Format-Table -AutoSize
+
+Export-Csv -Path "C:\Temp\Usuarios.csv" -NoTypeInformation -Encoding UTF8
---
+++ VerificaSerialNumber.ps1
@@ -0,0 +1,4 @@
+﻿$computerName = "ADM_RH04"
+$opcoes = New-CimSessionOption -Protocol Dcom
+$sessao = New-CimSession -ComputerName $computerName -SessionOption $opcoes
+Get-CimInstance -ClassName win32_bios -CimSession $sessao | Format-List SerialNumber
